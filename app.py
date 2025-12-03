6372bf170661659c58e30920ddd4d3f4b05f340b
nikhil230405-hue <nikhil230405@gmail.com>
2025-12-03 16:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ Question: {question}
 # ----------------------------
 # STREAMLIT UI
 # ----------------------------
-st.title("📘 Simple University FAQ RAG Chatbot (Groq + FAISS)")
+st.title("📘 University FAQ RAG Chatbot (Groq + FAISS)")
 st.write("Upload your PDF and ask any question.")
 
 pdf = st.file_uploader("Upload your FAQ PDF", type="pdf")
